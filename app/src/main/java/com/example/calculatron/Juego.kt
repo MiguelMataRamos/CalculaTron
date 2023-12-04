4b7e866573bf73553ccd3e58d759f11dd0a1e0c9
EAG <mataramosmiguel@gmail.com>
2023-12-04 19:58:31 +0100

    noseeeeeee

--- app/src/main/java/com/example/calculatron/Juego.kt
+++ app/src/main/java/com/example/calculatron/Juego.kt
@@ -12,20 +12,24 @@ class Juego : AppCompatActivity() {
 
     private lateinit var sp : SharedPreferences
     private lateinit var bind : ActivityJuegoBinding
-    var contador = 20
+    private var contador = 20
+    private var acertadas = 0
+    private var falladas = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         bind = ActivityJuegoBinding.inflate(layoutInflater)
         setContentView(bind.root)
-
         sp = PreferenceManager.getDefaultSharedPreferences(this)
+
         contador = sp.getString("cuenta_atras","20")!!.toInt()
+        bind.textContador.text = contador.toString()
+
+        bind.textoAcertadas.text = getString(R.string.acertadas_juego, acertadas)
+        bind.textoFalladas.text = getString(R.string.falladas_juego, falladas)
 
     }
 
-    private fun actualizarContador() {
-        bind.textContador.setText("Contador: $contador")
-    }
+
 
 
     fun ajustes(view: View) {
