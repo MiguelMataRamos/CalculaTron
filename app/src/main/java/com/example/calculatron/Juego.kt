759940e7b6e1cf0e71843a43ded436b23b3c9047
EAG <mataramosmiguel@gmail.com>
2023-12-04 18:51:06 +0100

    noseeee

--- app/src/main/java/com/example/calculatron/Juego.kt
+++ app/src/main/java/com/example/calculatron/Juego.kt
@@ -1,11 +1,27 @@
 package com.example.calculatron
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.calculatron.databinding.ActivityJuegoBinding
 
 class Juego : AppCompatActivity() {
+
+    private lateinit var bind : ActivityJuegoBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_juego)
+        bind = ActivityJuegoBinding.inflate(layoutInflater)
+        setContentView(bind.root)
     }
+
+    fun ajustes(view: View) {
+        var intent = Intent(this,Configuracion::class.java)
+        startActivity(intent)
+    }
+
+
+
+
+
 }
